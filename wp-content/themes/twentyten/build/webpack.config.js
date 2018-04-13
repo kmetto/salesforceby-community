5010da253694a4ec0fd9893812ecafd32c8b0662
mashagrand4 <mashagrand4@gmail.com>
2018-04-13 11:52:21 +0300

    Added base webpack config

---
+++ wp-content/themes/twentyten/build/webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import CleanWebpackPlugin from 'clean-webpack-plugin';
+// import NodemonPlugin from 'nodemon-webpack-plugin';
+// import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+module.exports =  {
+    entry: {
+        main: path.resolve(__dirname, '../src/frontend/js/search-hidden.js'),
+    },
+    // resolve: {
+    //     alias: {
+    //         '@root': path.resolve(__dirname, '.'),
+    //         '@frontend': path.resolve(__dirname, 'src/frontend'),
+    //     },
+    // },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'js/[name].bundle.js',
+        publicPath: 'dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                // use: {
+                //     loader: 'babel-loader',
+                //     options: {
+                //         presets: ['@babel/preset-env'],
+                //     },
+                // },
+            },
+            // {
+            //     test: /\.scss$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap'],
+            //     }),
+            // },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images',
+                    },
+                },
+            },
+            {
+                test: /\.ttf$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts',
+                        publicPath: 'dist/fonts/',
+                    },
+                },
+            },
+        ],
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         filename: 'login.html',
+    //         template: 'src/backend/views/login.html',
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         filename: 'registration.html',
+    //         template: 'src/backend/views/reg.html',
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         filename: 'index.html',
+    //         template: 'src/backend/views/index.html',
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         filename: 'profile.hbs',
+    //         template: 'src/backend/views/profile.hbs',
+    //     }),
+    //     new CleanWebpackPlugin('../dist'),
+    //     new NodemonPlugin({
+    //         watch: [
+    //             path.resolve('./src'),
+    //             path.resolve('./app.js'),
+    //         ],
+    //         script: './app.js',
+    //         execMap: {
+    //             js: 'babel-node',
+    //         },
+    //         legacyWatch: true,
+    //     }),
+    //     new ExtractTextPlugin('styles/styles.css'),
+    // ],
+};
