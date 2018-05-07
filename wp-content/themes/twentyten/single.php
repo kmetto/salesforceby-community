9b796df6b5e53c0dfb4a4a30247564beeee31e0e
Konstantin Metto <met9127@gmail.com>
2018-05-07 11:21:37 +0300

    fiexd single post layoyt

--- wp-content/themes/twentyten/single.php
+++ wp-content/themes/twentyten/single.php
@@ -1,6 +1,12 @@
 <?php
 /**
- * Template for displaying all single posts
+ * Main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
  * @subpackage Twenty_Ten
@@ -9,20 +15,20 @@
 
 get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+    <div class="main">
+        <div class="main__posts" role="main">
 
-			<?php
-			/*
-			 * Run the loop to output the post.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-single.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'single' );
-			?>
-
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
+        <?php
+        /*
+         * Run the loop to output the posts.
+         * If you want to overload this in a child theme then include a file
+         * called loop-index.php and that will be used instead.
+         */
+        get_template_part( 'loop', 'single' );
+        ?>
+        </div><!-- #content -->
+        <div class="main__sidebar">
+            <?php get_sidebar(); ?>
+        </div>
+    </div><!-- #container -->
 <?php get_footer(); ?>
