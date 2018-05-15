5a14de6de5a8eee5a42262cf5b6c5e9150bff720
Konstantin Metto <met9127@gmail.com>
2018-05-15 12:24:57 +0300

    added template for user page

---
+++ wp-content/themes/twentyten/templates/page-user-info.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: User Info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div class="main">
+    <div class="main__posts" role="main">
+
+    <?php
+    /*
+     * Run the loop to output the posts.
+     * If you want to overload this in a child theme then include a file
+     * called loop-index.php and that will be used instead.
+     */
+    get_template_part( 'loop', 'single' );
+    ?>
+    </div><!-- #content -->
+    <div class="main__sidebar">
+        <?php get_sidebar(); ?>
+    </div>
+</div><!-- #container -->
+<?php get_footer(); ?>
